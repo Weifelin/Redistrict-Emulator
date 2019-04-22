0d3791bb3ab934feec0d2f43b8088742a36e43bb
weifelin <lin.wfxz@gmail.com>
2019-04-22 07:15:26 -0400

    Initial commit

---
+++ src/main/java/com/giant/demo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.giant.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "index";
+    }
+
+}
