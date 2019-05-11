b76033d245478f59cbc854589216d5bd48131519
wwalters357 <wwalters357@gmail.com>
2019-05-11 18:39:17 -0400

    init edges

--- src/main/java/com/giant/demo/DemoApplication.java
+++ src/main/java/com/giant/demo/DemoApplication.java
@@ -37,7 +37,7 @@ public class DemoApplication extends SpringBootServletInitializer {
 
 	
 	public static void main(String[] args) {
-		PreProcess data = new PreProcess();
+		//PreProcess data = new PreProcess();
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
